f2848a41622c83f33ba621eac618de4edc36ca2b
Allwin Williams Samuvel <allwinwilliams.info@gmail.com>
2017-01-01 06:30:09 +0530

    minor changes

--- test.py
+++ test.py
@@ -8,75 +8,23 @@ import keyboard
 import Cpyq
 import functions
 
-# print("test")
 cpy_obj=Cpyq.Cpyq()
-# for i in range(10):
-#     x=(i*i)%(i+3)
-#     print(x,"\n")
-#     c.add_front(x)
-# for i in range(10):
-#     print(c.del_front())
-#
-
-
-# def signal_handler(sig, frame):
-#         print('You pressed Ctrl+C!')
-#         sys.exit(0)
-#
-# signal.signal(signal.SIGINT, signal_handler)
-# print('Press Ctrl+C')
-# signal.pause()
-
-
-    # sys.path.append(os.path.abspath("SO_site-packages"))
-    #
-    # recent_value = ""
-    #
-    # while True:
-    #     tmp_value = functions.read_from_clipboard()
-    #     if tmp_value != recent_value:
-    #         recent_value = tmp_value
-    #         Cpyq.add_front(recent_value)
-    #
-
-
 
 while True:
 
     try:
 
-        if sys.platform == "linux" or sys.platform == "linux2":
-
-            if keyboard.is_pressed('ctrl++shift+c'):
-                print('You Pressed ctrl shift c!')
-
-            elif keyboard.is_pressed('ctrl+shift+v'):
-                print('You Pressed ctrl shift c!')
-
-            elif keyboard.is_pressed('ctrl+c'):
-                print('You Pressed ctrl c!')
-
-            elif keyboard.is_pressed('ctrl+v'):
-                print('You Pressed ctrl v!')
-
-            elif keyboard.is_pressed('ctrl+z'):
-                print('You Pressed ctrl z!')
-
-            else:
-                pass
+        if sys.platform == "darwin":
 
 
-        elif sys.platform == "darwin":
-
-
-            if keyboard.is_pressed('cmd+shift+c'):
+            if keyboard.is_pressed('cmd+alt+c'):
                 print('You Pressed cmd shift c!')
                 x=functions.read_from_clipboard()
                 print(x)
                 cpy_obj.add_front(x)
                 keyboard.write("")
 
-            elif keyboard.is_pressed('cmd+shift+v'):
+            elif keyboard.is_pressed('cmd+alt+v'):
                 print('You Pressed cmd shift v!')
                 print(cpy_obj.del_front())
                 keyboard.write("")
@@ -96,12 +44,17 @@ while True:
                 # keyboard.write("delete")
                 keyboard.write(x)
                 keyboard.write("")
+
             elif keyboard.is_pressed('cmd+z'):
                 print('You Pressed cmd z!')
                 keyboard.write("")
 
             else:
                 pass
+
+        elif sys.platform == "linux" or sys.platform == "linux2":
+            pass
+
         else:
             pass
 
