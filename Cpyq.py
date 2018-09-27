e1e57f044ccba34575d1162df0c79a326e61b9f9
Allwin Williams Samuvel <allwinwilliams.info@gmail.com>
2018-09-27 08:23:39 +0530

    working on corrections

--- Cpyq.py
+++ Cpyq.py
@@ -2,6 +2,16 @@ class Cpyq():
 
     def __init__(self):
         self.Q=[]
+        self.nq=["","","","","","","","",""]
+
+    def copy_nq(self,n,s):
+        self.nq[n-1]=s
+
+    def paste_nq(self,n):
+        return self.nq[n-1]
+
+    def clear(self):
+        self.Q.clear()
 
     def add_front(self, s):
         self.Q.insert(0,s)
@@ -10,7 +20,7 @@ class Cpyq():
         self.Q.append(s)
 
     def add_n(self, s, n):
-        if n<=len(Q):
+        if n<=len(self.Q):
             self.Q.insert(n-1,s)
 
     def del_front(self):
@@ -24,7 +34,7 @@ class Cpyq():
         return self.Q.pop()
 
     def del_n(self, n):
-        if n<=len(Q):
+        if n<=len(self.Q):
             return self.Q.pop(n-1)
         return None
 
@@ -32,9 +42,12 @@ class Cpyq():
         return self.Q[0]
 
     def get_back(self):
-        return self.Q[len(Q)-1]
+        return self.Q[len(self.Q)-1]
 
     def get_n(self, n):
-        if n<=len(Q):
+        if n<=len(self.Q):
             return self.Q[n-1]
         return None
+
+    def show(self):
+            print("Q",self.Q)
--- test.py
+++ test.py
@@ -16,38 +16,75 @@ while True:
 
         if sys.platform == "darwin":
 
+            if keyboard.is_pressed('+1+c'):
+                pass
+                
+            elif keyboard.is_pressed('+2+c'):
+                pass
 
-            if keyboard.is_pressed('cmd+alt+c'):
-                print('You Pressed cmd shift c!')
-                x=functions.read_from_clipboard()
-                print(x)
-                cpy_obj.add_front(x)
-                keyboard.write("")
+            elif keyboard.is_pressed('+1+v'):
+                pass
 
-            elif keyboard.is_pressed('cmd+alt+v'):
-                print('You Pressed cmd shift v!')
-                print(cpy_obj.del_front())
-                keyboard.write("")
-
-            elif keyboard.is_pressed('cmd+c'):
-                print('You Pressed cmd c!')
+            elif keyboard.is_pressed('alt+c'):
+                print('You Pressed alt c!')
                 x=functions.read_from_clipboard()
                 print("text to clipboard: ", x)
                 cpy_obj.add_front(x)
                 keyboard.write("")
                 functions.write_to_clipboard("")
+                cpy_obj.show()
 
-            elif keyboard.is_pressed('cmd+v'):
-                print('You Pressed cmd v!')
-                x=cpy_obj.del_back()
+            elif keyboard.is_pressed('alt+v'):
+                print('You Pressed ctrl v!')
+                x=cpy_obj.del_front()
                 print("to be pasted: ",x)
                 # keyboard.write("delete")
                 keyboard.write(x)
                 keyboard.write("")
+                cpy_obj.show()
+
+            if keyboard.is_pressed('ctrl+shift+c'):
+                print('You Pressed alt c!')
+                x=functions.read_from_clipboard()
+                print("text to clipboard: ", x)
+                cpy_obj.add_front(x)
+                keyboard.write("")
+                functions.write_to_clipboard("")
+                cpy_obj.show()
+
+            elif keyboard.is_pressed('ctrl+shift+v'):
+                print('You Pressed ctrl v!')
+                x=cpy_obj.del_front()
+                print("to be pasted: ",x)
+                # keyboard.write("delete")
+                keyboard.write(x)
+                keyboard.write("")
+                cpy_obj.show()
+
+
+            elif keyboard.is_pressed('cmd+c'):
+                print('You Pressed cmd c!')
+                x=functions.read_from_clipboard()
+                print("text to clipboard: ", x)
+                cpy_obj.add_back(x)
+                keyboard.write("")
+                functions.write_to_clipboard("")
+                cpy_obj.show()
+
+            elif keyboard.is_pressed('cmd+v'):
+                print('You Pressed cmd v!')
+                x=cpy_obj.get_back()
+                print("to be pasted: ",x)
+                # keyboard.write("delete")
+                keyboard.write(x)
+                keyboard.write("")
+                cpy_obj.show()
 
             elif keyboard.is_pressed('cmd+z'):
                 print('You Pressed cmd z!')
+                cpy_obj.clear()
                 keyboard.write("")
+                cpy_obj.show()
 
             else:
                 pass
